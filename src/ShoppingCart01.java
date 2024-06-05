db3a5d051f0bbb22dac14b26b94826f18f49da6c
Aluno <Aluno@10.85.198.10>
2024-06-05 16:41:39 -0300

    ShoppingCart01

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,9 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "Alex quer comprar uma ";
+        String itemDesc = "Camisa";
+        String message = custName + itemDesc;
+        System.out.println(message);
+    }
+}
